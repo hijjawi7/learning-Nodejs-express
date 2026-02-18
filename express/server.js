3cf0f0f5320a3562c40e9047a7ba175545ee6458
mohammad hijjawi <mohammad@goit.ps>
2026-02-18 09:46:17 +0200

    creat router, create function get, post, put, delete and test postman

--- express/server.js
+++ express/server.js
@@ -18,19 +18,59 @@ app.get('/',(req, res) => {
 });
 
 router.get('/',(req, res) => {
-    res.send('All cars');
+    res.json(cars);
 });
 
 router.post('/',(req, res) => {
-    res.send('New car');
+    const {make, model, year ,price} = req.body;
+
+    if(!make || !model || !year || !price){
+        return res.status(400).json({error: "Missing fields"});
+    }
+
+    const newCars = {
+        id: cars.length+1,
+        make,
+        model,
+        year: Number(year),
+        price: Number(price)
+    };
+
+    cars.push(newCars);
+    res.status(201).json(newCars)
 });
 
 router.put('/:id', (req, res) => {
-    res.send('Update car');
+
+    const id = Number(req.params.id);
+    const index = cars.findIndex(car => car.id === id);
+
+    if(index === -1){
+        return res.status(404).json({error: "Car Not Found"});
+    }
+
+    const {make, model, year ,price} = req.body;
+
+    if (make) cars[index].make = make;
+    if (model) cars[index].model = model;
+    if (year) cars[index].year = Number(year);
+    if (price) cars[index].price = Number(price);
+
+    res.json(cars[index]);
 });
 
 router.delete('/:id', (req, res) => {
-    res.send('Delete car');
+    const id = Number(req.params.id);
+    const index = cars.findIndex(car => car.id === id);
+
+    if (index === -1){
+        return res.status(404).json({error: "Car Not Found"});
+    }
+
+    const deleted = cars.splice(index, 1)[0];
+
+    res.json({message: "Car Deleted", car: deleted});
+
 });
 
 router.get('/:id', (req, res) => {
